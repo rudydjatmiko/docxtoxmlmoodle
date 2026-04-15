f8ce3a7837c7ffca4b34a0b4001f73ca4acb82f7
rudydjatmiko <rudydjatmiko71@guru.smp.belajar.id>
2026-04-15 12:16:53 +0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
-from core.parser_engine import parse
-from processors.content_processor import process
+from docxtoxmlmoodle.core.parser_engine import parse
+from docxtoxmlmoodle.processors.content_processor import process
 
 def run_parser(elements):
     elements = process(elements)
