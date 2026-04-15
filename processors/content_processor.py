04a57eeeac0c4bd690402dcb8f70a8ad20c63943
rudydjatmiko <rudydjatmiko71@guru.smp.belajar.id>
2026-04-15 11:56:46 +0700

    Update content_processor.py

--- processors/content_processor.py
+++ processors/content_processor.py
@@ -1,13 +1,13 @@
-from processors.image_handler import extract_images
-
 def process(elements):
+    # SEKARANG tidak perlu extract_images lagi di sini
+    # karena image sudah di-handle di docx_reader
+
     processed = []
 
     for el in elements:
-        item = {
+        processed.append({
             "text": el.get("text", ""),
-            "images": extract_images(el)
-        }
-        processed.append(item)
+            "images": el.get("images", [])
+        })
 
     return processed
