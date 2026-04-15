8c6e4fcb5ef09f35341cf4dec9c0958e33151906
rudydjatmiko <rudydjatmiko71@guru.smp.belajar.id>
2026-04-15 11:53:04 +0700

    Update image_handler.py

--- processors/image_handler.py
+++ processors/image_handler.py
@@ -1,17 +1,43 @@
 import base64
-import uuid
 
-def extract_images(el):
+def get_image_map(doc):
+    """
+    mapping rId → image part
+    """
+    rels = doc.part._rels
+    image_map = {}
+
+    for rel in rels:
+        rel = rels[rel]
+        if "image" in rel.target_ref:
+            image_map[rel.rId] = rel.target_part
+
+    return image_map
+
+
+def extract_images_from_paragraph(paragraph, image_map):
+    """
+    extract image dari 1 paragraf
+    """
     images = []
 
-    for img_xml in el.get("images", []):
-        # dummy handler (real extraction bisa di-upgrade)
-        name = f"{uuid.uuid4().hex}.png"
-        data = base64.b64encode(img_xml.encode()).decode()
+    # cari semua embed rId
+    blips = paragraph._element.xpath('.//a:blip')
 
-        images.append({
-            "name": name,
-            "data": data
-        })
+    for blip in blips:
+        rId = blip.get('{http://schemas.openxmlformats.org/officeDocument/2006/relationships}embed')
+
+        if rId in image_map:
+            part = image_map[rId]
+
+            image_bytes = part.blob
+            filename = part.partname.split("/")[-1]
+
+            encoded = base64.b64encode(image_bytes).decode("utf-8")
+
+            images.append({
+                "name": filename,
+                "data": encoded
+            })
 
     return images
