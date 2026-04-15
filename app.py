cd47faf1d13ff08c4d5cd351416b1096ab70acbe
rudydjatmiko <rudydjatmiko71@guru.smp.belajar.id>
2026-04-15 11:59:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
+import streamlit as st
 from core.docx_reader import read_docx
 from parser import run_parser
 from core.builder import build_xml
 
-INPUT = "input.docx"
-OUTPUT = "output.xml"
+st.title("DOCX → Moodle XML Converter")
 
-def main():
-    print("📥 Reading DOCX...")
-    elements = read_docx(INPUT)
+uploaded_file = st.file_uploader("Upload file DOCX", type=["docx"])
 
-    print("⚙️ Parsing...")
+if uploaded_file:
+    with open("temp.docx", "wb") as f:
+        f.write(uploaded_file.read())
+
+    st.success("File uploaded!")
+
+    # PROCESS
+    elements = read_docx("temp.docx")
     questions = run_parser(elements)
-
-    print("🧾 Building XML...")
     xml = build_xml(questions)
 
-    with open(OUTPUT, "w", encoding="utf-8") as f:
-        f.write(xml)
-
-    print("✅ DONE:", OUTPUT)
-
-if __name__ == "__main__":
-    main()
+    st.download_button(
+        label="Download XML",
+        data=xml,
+        file_name="result.xml",
+        mime="text/xml"
+    )
