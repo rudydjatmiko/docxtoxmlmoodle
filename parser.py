25bfb8e68ff53ed2eeb593201b537fa01f91853f
rudydjatmiko <rudydjatmiko71@guru.smp.belajar.id>
2026-05-04 15:20:46 +0700

    Delete parser.py

--- parser.py
+++
@@ -1,8 +0,0 @@
-from core.hybrid_reader import read_docx_hybrid
-from core.parser_engine import parse
-
-
-def run_parser(path):
-    elements = read_docx_hybrid(path)
-    questions = parse(elements)
-    return questions, elements
